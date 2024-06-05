7df1025783dbb438f8a45335f21672818fc47e12
timone019 <143154960+timone019@users.noreply.github.com>
2024-06-05 15:39:24 -0700

    6.4: updated movie card html & component - passed movie._id in the toggleFavorite for moviecard html, specified to include it to favorite movie by movie._id  fixed the user response to res.body

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -5,6 +5,11 @@ import { FavoriteMoviesService } from '../favorite-movies.service';
 
 interface User {
   Username: string;
+  Password: string;
+  Email: string;
+  Birthday: Date;
+  FullName: string;
+  FavoriteMovies: string[];
 }
 
 @Component({
@@ -12,7 +17,7 @@ interface User {
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.scss',
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
   user: any = {};
   favorites: any[] = [];
@@ -23,62 +28,82 @@ export class MovieCardComponent {
   ) {}
 
   ngOnInit(): void {
-    this.getMovies();
+    // this.favoriteMoviesService.favoriteMovies$.subscribe((movies: any[]) => {
+    //   this.favorites = movies;
+    // });
     this.getUser();
+  }
+
+
+  getUser(): void {
+    const { Username } = JSON.parse(localStorage.getItem('currentUser') || '{}');
+  
+    if (!Username) {
+      console.error('No user found');
+      return;
+    }
+  
+    this.fetchApiData.getUser(Username).subscribe((res: any) => {
+      this.user = res.body
+      console.log(this.user);
+      if (Array.isArray(this.user.FavoriteMovies)) {
+        this.favorites = this.user.FavoriteMovies;
+        this.favoriteMoviesService.updateFavoriteMovies(this.favorites);
+      }
+      this.getMovies();
+    });
   }
 
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
+      this.updateFavoriteStatus();
       return this.movies;
     });
   }
 
-  toggleFavorite(movie: any) {
-    movie.isFavorite = !movie.isFavorite;
-    movie.heartActive = movie.isFavorite; 
-  
-    const currentUser: User | null = JSON.parse(localStorage.getItem('currentUser') || '{}');
-    const username = currentUser ? currentUser.Username : '';
-  
-    console.log('currentUser:', currentUser);
-    console.log('username:', username);
-  
-    if (!username) {
-      console.error('No user found');
-      return;
-    }
-    
-    if (movie.isFavorite) {
-      // If the movie was favorited, add it to the user's favorite movie list
-      this.fetchApiData.addFavoriteMovie(username, movie._id).subscribe(() => {
-        console.log(`Added ${movie.Title} to favorites`);
-      });
-    } else {
-      // If the movie was unfavorited, remove it from the user's favorite movie list
-      this.fetchApiData.deleteFavoriteMovie(username, movie._id).subscribe(() => {
-        console.log(`Removed ${movie.Title} from favorites`);
-      });
-    }
-  
-
-
+  updateFavoriteStatus(): void {
+    this.movies.forEach(movie => {
+      movie.isFavorite = this.favorites.includes(movie._id);
+      movie.heartActive = movie.isFavorite;
+    });
   }
-  getUser(): void {
-    const { Username } = JSON.parse(localStorage.getItem('currentUser') || '{}');
+  
+  toggleFavorite(selectedMovie: any) {
+  // selectedMovie.isFavorite = !movie.isFavorite;
+  // movie.heartActive = movie.isFavorite;
 
-    if (!Username) {
-      console.error('No user found');
-      return;
-    }
+  const currentUser: User | null = JSON.parse(localStorage.getItem('currentUser') || '{}');
+  const username = currentUser ? currentUser.Username : '';
 
-    this.fetchApiData.getUser(Username).subscribe((res: any) => {
-      this.user = res;
-      this.favorites = this.user.FavoriteMovies;
+  console.log('currentUser:', currentUser);
+  console.log('username:', username);
+
+  if (!username) {
+    console.error('No user found');
+    return;
+  }
+
+  if (currentUser && currentUser.FavoriteMovies.includes(selectedMovie)) {
+    // If the movie was favorited, add it to the user's favorite movie list
+    this.fetchApiData.addFavoriteMovie(username, selectedMovie).subscribe(() => {
+      console.log(`Added ${selectedMovie} to favorites`);
+      this.favorites.push(selectedMovie); // this is adding movie to local storage favorite movie list causing dupes, need to check 
+      this.favoriteMoviesService.updateFavoriteMovies(this.favorites);
+    });
+  } else {
+    // If the movie was unfavorited, remove it from the user's favorite movie list
+    this.fetchApiData.deleteFavoriteMovie(username, selectedMovie).subscribe(() => {
+      console.log(`Removed ${selectedMovie} from favorites`);
+      const index = this.favorites.indexOf(selectedMovie);
+      if (index > -1) {
+        this.favorites.splice(index, 1);
+      }
       this.favoriteMoviesService.updateFavoriteMovies(this.favorites);
     });
   }
+}
   
   
   // openGenreModal(movie: Movie): void {
